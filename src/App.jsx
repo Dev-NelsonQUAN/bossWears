38395412716825ccec9cd88d7e3e730c82bf6d29
Dell E5440 i5 <nelson.ade.quadri@gmail.com>
2024-10-11 16:43:04 +0100

    Resolved

--- src/App.jsx
+++ src/App.jsx
@@ -7,48 +7,47 @@ import AdminLogin from './components/auth/AdminLogin/AdminLogin'
 import Cart from './components/Pages/Cart/Cart'
 import ForgotPassword from './components/auth/ForgotPassword/ForgotPassword'
 import ResetPassword from './components/auth/ResetPassword/ResetPassword'
-
+import AddItem from './components/Pages/AddItem/AddItem'
 
 const router = createHashRouter([
-
   {
     path: "/",
-    element: <LandingPage/>,
+    element: <LandingPage />,
   },
   {
     path: "/forgot",
-    element: <ForgotPassword/>
+    element: <ForgotPassword />
   },
   {
     path: "/reset",
-    element: <ResetPassword/>
+    element: <ResetPassword />
   },
   {
     path: "/admin-login",
-    element: <AdminLogin/>
+    element: <AdminLogin />
   },
   {
     path: "/add-item",
-    element: <AddItem/>
+    element: <AddItem />
   },
   {
-    path:"/details/:id",
-    element: <Details/>
+    path: "/details/:id",
+    element: <Details />
   },
   {
     path: "/order",
-    element: <Order/>
+    element: <Order />
   },
   {
     path: "/cart",
-    element: <Cart/>
+    element: <Cart />
   }
 ])
 
 const App = () => {
   return (
     <>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </>
   )
 }
